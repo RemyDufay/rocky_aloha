726ed59b258367fddfc5b83cb5681e2d3a35d2d6
Remy Dufay <remy.dufay@gmail.com>
2022-05-24 00:26:51 +0200

    Requests: Creation du fichier de migration pour ajout propre de la classe : OK. Cohérence rétablie entre les fichiers de migration et le schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_142206) do
+ActiveRecord::Schema.define(version: 2022_05_23_222512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "requests", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "rock_id", null: false
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["rock_id"], name: "index_requests_on_rock_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
 
   create_table "rocks", force: :cascade do |t|
     t.string "color"
@@ -42,5 +53,7 @@ ActiveRecord::Schema.define(version: 2022_05_23_142206) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "requests", "rocks"
+  add_foreign_key "requests", "users"
   add_foreign_key "rocks", "users"
 end
