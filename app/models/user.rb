215770a96a8c20d969c3707d218f04483d8206b5
Remy Dufay <remy.dufay@gmail.com>
2022-05-23 17:39:55 +0200

    Finalisation des modeles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :rocks
+  has_many :requests
+
 end
