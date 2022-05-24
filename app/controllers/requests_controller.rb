3ca6a85a1b4095c994fb4addb31475e39309b9b7
Remy Dufay <remy.dufay@gmail.com>
2022-05-24 23:18:33 +0200

    Creation du form Request, de la show Request. Amelioration de la show Rock (ajout de l'image)

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,29 @@
+class RequestsController < ApplicationController
+  def index
+    @requests = Request.all
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @rock = Rock.find(params[:rock_id])
+    @user = current_user
+    @request.rock = @rock
+    @request.user = @user
+    @request.save!
+    redirect_to request_path(@request)
+
+  end
+
+
+
+  def show
+    @request = Request.find(params[:id])
+  end
+
+
+  private
+
+  def request_params
+    params.require(:request).permit(:date)
+  end
+end
