53458ff921605ddf5f88349b58b034f287e6fd16
[A[A[A[B[B[A[A <antoine.mgy.mnc@gmail.com>
2022-05-24 17:14:19 +0200

    Pending the new pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
 
 @acanthite= User.create!(email: 'acanthite.cohénite@gmail.com', first_name: 'lola' , last_name: 'schreibersite' , address: 'Nantes' , password: "orthorhombique")
 @galène= User.create!(email: 'galène.caillou@gmail.com', first_name: 'galène' , last_name: 'coquillart' , address: '33 Alee Du Mole, Cessy, France' , password: "érubescite")
@@ -23,9 +25,13 @@
 
 @caillou = Rock.create!(user: @celsiane, color: 'black', shape: 'round'  , name: 'caillou laurent', price: 10, description: "compagnon pour vos soirées aura les meilleures blagues ")
 
-Rock.create!(user: @mica, color: 'grey', shape: 'transparent'  , name: 'lola pierre de conversation', price: 206, description: "vous pouvez vous confier sans honte , vous pouvez baisser votre masque , gardera vos secrets pour toujours , ne vous interrompt pas ")
+@pikachu = Rock.create!(user: @mica, color: 'grey', shape: 'transparent'  , name: 'lola pierre de conversation', price: 206, description: "vous pouvez vous confier sans honte , vous pouvez baisser votre masque , gardera vos secrets pour toujours , ne vous interrompt pas ")
+@pikachu_img = URI.open("https://res.cloudinary.com/dwcmq45ak/image/upload/v1653384553/Capture_d_e%CC%81cran_2022-05-23_a%CC%80_15.55.38_kj2x55.png")
+@pikachu.photo.attach(io: @pikachu_img, filename: 'nes.jpg', content_type: 'image/png')
 
-Rock.create!(user: @barite, color: 'grey', shape: 'big'  , name: 'bertrand le robuste ', price: 334, description: "garde du corps , poids musculation  , un seul lancer eloigne les haters  ")
+@jeaneude = Rock.create!(user: @barite, color: 'grey', shape: 'big'  , name: 'bertrand le robuste ', price: 334, description: "garde du corps , poids musculation  , un seul lancer eloigne les haters  ")
+@jeaneude_img = URI.open("https://res.cloudinary.com/dwcmq45ak/image/upload/v1653384553/Capture_d_%C3%A9cran_2022-05-23_%C3%A0_15.56.00_drjawd.png")
+
 
 Rock.create!(user: @toto, color: 'green', shape: 'round'  , name: 'raavi', price: 422, description: "vous aligne les chakras , détend votre corps   ")
 
